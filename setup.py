bb14e0250fa02053a4b4bc23f3ed18a78d5eac4b
Sorokin Alexei <sor.alexei@meowr.ru>
2016-06-09 16:57:23 +0300

    Make files to install list prettier

--- setup.py
+++ setup.py
@@ -51,37 +51,56 @@ def datafilelist(installbase, sourcebase):
     return datafileList
 
 data_files = [
-    ('{prefix}/share/man/man1'.format(prefix=sys.prefix), glob('data/*.1')),
-    ('{prefix}/share/glib-2.0/schemas'.format(prefix=sys.prefix), ['data/org.mate.mate-menu.gschema.xml',
-                                                                  ] +
-                                                                  glob('data/plugins/org.mate.mate-menu.plugins.*.gschema.xml'),
-    ),
-    ('{prefix}/share/mate-panel/applets'.format(prefix=sys.prefix), ['data/org.mate.panel.MateMenuApplet.mate-panel-applet']),
-    ('{prefix}/share/dbus-1/services'.format(prefix=sys.prefix), ['data/org.mate.panel.applet.MateMenuAppletFactory.service']),
-    ('{prefix}/share/mate-menu'.format(prefix=sys.prefix), ['data/mate-menu-config.glade',
-                                                            'data/mate-menu.glade',
-                                                            'data/popup.xml',
-                                                            'data/applications.list',
-                                                           ],
-    ),
-    ('{prefix}/share/mate-menu/plugins'.format(prefix=sys.prefix), ['data/plugins/applications.glade',
-                                                                    'data/plugins/system_management.glade',
-                                                                    'data/plugins/places.glade',
-                                                                    'data/plugins/recent.glade',
-                                                                    'mate_menu/plugins/applications.py',
-                                                                    'mate_menu/plugins/system_management.py',
-                                                                    'mate_menu/plugins/places.py',
-                                                                    'mate_menu/plugins/recent.py',
-                                                                   ],
-    ),
-    ('{prefix}/share/mate-menu/icons'.format(prefix=sys.prefix), ['data/icons/mate-logo.svg',
-                                                                  'data/icons/dictionary.png',
-                                                                 ],
-    ),
-    ('{prefix}/share/mate-menu/icons/search_engines'.format(prefix=sys.prefix), ['data/icons/google.ico','data/icons/wikipedia.ico']),
-    ('{prefix}/lib/mate-menu'.format(prefix=sys.prefix), glob('lib/*.py')),
+  ("share/man/man1",
+   glob("data/*.1")
+  ),
+  ("share/glib-2.0/schemas",
+   [
+    "data/org.mate.mate-menu.gschema.xml",
+   ] +
+   glob("data/plugins/org.mate.mate-menu.plugins.*.gschema.xml"),
+  ),
+  ("share/mate-panel/applets",
+   [
+    "data/org.mate.panel.MateMenuApplet.mate-panel-applet",
+   ]),
+  ("share/dbus-1/services",
+   [
+    "data/org.mate.panel.applet.MateMenuAppletFactory.service",
+   ]),
+  ("share/mate-menu",
+   [
+    "data/mate-menu.glade",
+    "data/mate-menu-config.glade",
+    "data/popup.xml",
+    "data/applications.list",
+   ]),
+  ("share/mate-menu/plugins",
+   [
+    "data/plugins/applications.glade",
+    "data/plugins/system_management.glade",
+    "data/plugins/places.glade",
+    "data/plugins/recent.glade",
+    "mate_menu/plugins/applications.py",
+    "mate_menu/plugins/system_management.py",
+    "mate_menu/plugins/places.py",
+    "mate_menu/plugins/recent.py",
+   ]),
+  ("share/mate-menu/icons",
+   [
+    "data/icons/mate-logo.svg",
+    "data/icons/dictionary.png",
+   ]),
+  ("share/mate-menu/icons/search_engines",
+   [
+    "data/icons/google.ico",
+    "data/icons/wikipedia.ico",
+   ]),
+  ("lib/mate-menu",
+   glob("lib/*.py"),
+  ),
 ]
-data_files.extend(datafilelist('{prefix}/share/locale'.format(prefix=sys.prefix), 'build/mo'))
+data_files.extend(datafilelist("share/locale", 'build/mo'))
 
 if sys.argv[1] in ("install", "uninstall"):
     # Enforce "/usr" prefix.
