153df650e168cb0822d6792e3f5775f652077624
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2019-08-02 17:59:38 +0200

    setup.py: Avoid installation of duplicate .py files already available in dist-packages into mate-menu's datadir.

--- setup.py
+++ setup.py
@@ -82,10 +82,6 @@ data_files = [
             "data/plugins/system_management.glade",
             "data/plugins/places.glade",
             "data/plugins/recent.glade",
-            "mate_menu/plugins/applications.py",
-            "mate_menu/plugins/system_management.py",
-            "mate_menu/plugins/places.py",
-            "mate_menu/plugins/recent.py",
         ]
     ),
     ("share/mate-menu/icons".format(prefix=sys.prefix),
